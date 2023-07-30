6edbe54cdcf4afffc7be55838e7501c04df11992
dvgitit <58548665+dvgitit@users.noreply.github.com>
2023-07-30 16:26:05 +0200

    Update kcsrv.sh

--- kcsrv.sh
+++ kcsrv.sh
@@ -1,1 +1,2 @@
-docker run -d -p 5151:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name keycloak jboss/keycloak
+#docker run -d -p 5151:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name keycloak jboss/keycloak
+curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
